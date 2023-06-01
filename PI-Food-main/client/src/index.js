ca863de0a793e849029bd8f06bd95d741fb31e07
Natalia Malvicino <natalia.malvicino@gmail.com>
2023-05-31 23:41:25 -0300

    Deploying Front 2

--- PI-Food-main/client/src/index.js
+++ PI-Food-main/client/src/index.js
@@ -8,7 +8,7 @@ import store from "./redux/store";
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:3001";
-axios.defaults.baseURL = "https://foodsapp.up.railway.app";
+axios.defaults.baseURL = "https://foodsapp-back.up.railway.app";
 
 //? BROWSER ROUTER AND PROVIDER FOR APP
 ReactDOM.render(
